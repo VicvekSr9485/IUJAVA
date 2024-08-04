cb821176dad90673730f69a3c92d59b086e192fb
VicvekSr <user@VicvekSr-MacBook-Pro.local>
2024-08-04 15:46:07 +0100

    add array manipulation

--- OOPJ/MyArray.java
+++ OOPJ/MyArray.java
@@ -1,10 +1,18 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public  class MyArray {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Enter the number of elements for the array: ");
             int n = scanner.nextInt();
+
+            // Ensure that the number of elements is positive
+            if (n <= 0) {
+                System.out.println("The number of elements must be positive.");
+                scanner.close();
+                return;
+            }
 
             int[] myArray = new int[n];
 
@@ -26,6 +34,16 @@ public  class MyArray {
                     min = myArray[i];
                 }
             }
+
+            // In-built sort method for sorting
+            // Sort the array
+            //Arrays.sort(myArray);
+
+            // The smallest element is the first element in the sorted array
+            //int min = myArray[0];
+
+            // The largest element is the last element in the sorted array
+            //int max = myArray[myArray.length - 1];
             
             System.out.println("Smallest element: " + min);
             System.out.println("Biggest element: " + max);
